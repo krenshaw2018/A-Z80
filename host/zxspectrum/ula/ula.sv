502250a39e23783d0123b0c530cf8c1875dc37da
Goran Devic <gdevic@yahoo.com>
2014-12-21 12:36:37 -0600

    zxspectrum: Various improvements
    - Added "Turbo" speed mode switch (SW2)
    - Turbo speed is (3.5 MHz x 2 = 7.0 MHz)
    - LEDG5 lits when a key is being pressed
    - LEDG6 blinks when a speaker/line-in is being toggled
    
    Misc:
    - Fixed a false clock for ula:beep
    - Fixed unused Kempston input warning
    - Updated documentation to 1.1

--- host/zxspectrum/ula/ula.sv
+++ host/zxspectrum/ula/ula.sv
@@ -22,6 +22,7 @@ module ula
     //-------- Clocks and reset -----------------
     input wire CLOCK_27,            // Input clock 27 MHz
     input wire CLOCK_24,            // Input clock 24 MHz
+    input wire turbo,               // Turbo speed (3.5 MHz x 2 = 7.0 MHz)
     output wire clk_vram,
     input wire reset,               // KEY0 is reset
     output wire locked,             // PLL is locked signal
@@ -42,6 +43,7 @@ module ula
     //-------- PS/2 Keyboard --------------------
     input wire PS2_CLK,
     input wire PS2_DAT,
+    output wire pressed,
 
     //-------- Audio (Tape player) --------------
     inout wire I2C_SCLK,
@@ -52,6 +54,7 @@ module ula
     output wire AUD_BCLK,
     output wire AUD_DACDAT,
     input wire AUD_ADCDAT,
+    output reg beeper,
 
     //-------- VGA connector --------------------
     output wire [3:0] VGA_R,
@@ -90,6 +93,8 @@ begin
         // Let us hear the tape loading!
         pcm_outl[12] <= pcm_inl[14] | pcm_inr[14];
         pcm_outr[12] <= pcm_inl[14] | pcm_inr[14];
+        // Let us see the tape loading!
+        beep <= (pcm_inl[14] | pcm_inr[14]) ^ D[4] ^ D[3];
     end
 end
 
@@ -110,6 +115,15 @@ reg  [15:0] pcm_outr;
 i2s_intf i2s_intf_( .CLK(CLOCK_24), .nRESET(reset),
     .PCM_INL(pcm_inl[15:0]), .PCM_INR(pcm_inr[15:0]), .PCM_OUTL(pcm_outl[15:0]), .PCM_OUTR(pcm_outr[15:0]),
     .I2S_MCLK(AUD_XCK), .I2S_LRCLK(AUD_ADCLRCK), .I2S_BCLK(AUD_BCLK), .I2S_DOUT(AUD_DACDAT), .I2S_DIN(AUD_ADCDAT) );
+
+// Show the beeper visually by dividing the frequency with some factor to generate blinks
+reg beep;                           // Beeper latch
+reg [6:0] beepcnt;                  // Beeper counter
+always @(posedge beep)
+begin
+    beepcnt <= beepcnt - '1;
+    if (beepcnt==0) beeper <= ~beeper;
+end
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Instantiate ULA's video subsystem
--- host/zxspectrum/zxspectrum_board.sv
+++ host/zxspectrum/zxspectrum_board.sv
@@ -70,8 +70,10 @@ module zxspectrum_board
     //-------- Misc and debug -------------------
     input wire SW0,                 // ROM selection
     input wire SW1,                 // Enable/disable interrupts
-    output wire [1:0] LEDR,         // Shows the switch selection
-    inout wire [31:0] GPIO_1
+    input wire SW2,                 // Turbo speed (3.5 MHz x 2 = 7.0 MHz)
+    output wire [2:0] LEDR,         // Shows the switch selection
+    inout wire [31:0] GPIO_1,
+    output wire [2:0] LEDGTOP       // Show additional information visually
 );
 `default_nettype none
 
@@ -84,6 +86,11 @@ assign GPIO_1[15:0] = A[15:0];
 assign GPIO_1[23:16] = D[7:0];
 assign GPIO_1[31:24] = {nM1,nMREQ,nIORQ,nRD,nWR,nRFSH,nHALT,nBUSACK};
 //assign GPIO_1[] = {nWAIT,nINT,nNMI,nRESET,nBUSRQ,CLK};
+
+// Top 3 green LEDs show various states:
+assign LEDGTOP[2] = 0;              // Reserved for future use
+assign LEDGTOP[1] = beeper;         // Show the beeper state
+assign LEDGTOP[0] = pressed;        // Show when a key is being pressed
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Internal buses and address map selection logic
@@ -183,11 +190,14 @@ assign SRAM_LB_N = 0;
 // Instantiate ULA
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 wire clk_cpu;                   // Global CPU clock of 3.5 MHz
+assign LEDR[2] = SW2;           // Glow red when in turbo mode (7.0 MHz)
 wire [12:0] vram_address;       // ULA video block requests a byte from the video RAM
 wire [7:0] vram_data;           // ULA video block reads a byte from the video RAM
 wire vs_nintr;                  // Generates a vertical retrace interrupt
+wire pressed;                   // Show that a key is being pressed
+wire beeper;                    // Show the beeper state
 
-ula ula_( .*, .clk_cpu(clk_cpu) );
+ula ula_( .*, .turbo(SW2), .clk_cpu(clk_cpu) );
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Instantiate A-Z80 CPU
@@ -203,7 +213,7 @@ wire nBUSACK;
 
 wire nWAIT = 1;
 wire nINT = (SW1==0)? vs_nintr : '1;// SW1 disables interrupts and, hence, keyboard
-assign LEDR[1] = SW1;               // Glow red when keyboard is *disabled*
+assign LEDR[1] = SW1;               // Glow red when interrupts are *disabled*
 wire nNMI = KEY1;                   // Pressing KEY1 issues a NMI
 wire nBUSRQ = 1;
 
@@ -212,10 +222,10 @@ z80_top_direct_n z80_( .*, .nRESET(reset), .CLK(clk_cpu) );
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Lit green LEDs to show activity on a Kempston compatible joystick
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-assign LEDG[0] = !kempston[5];  // UP
-assign LEDG[1] = !kempston[4];  // DOWN
-assign LEDG[2] = !kempston[0];  // LEFT
-assign LEDG[3] = !kempston[2];  // RIGHT
-assign LEDG[4] = !kempston[3];  // BUTTON
+assign LEDG[0] = !kempston[5];                  // UP
+assign LEDG[1] = !kempston[4];                  // DOWN
+assign LEDG[2] = !kempston[0];                  // LEFT
+assign LEDG[3] = !kempston[2];                  // RIGHT
+assign LEDG[4] = !kempston[3] | !kempston[1];   // BUTTON
 
 endmodule
